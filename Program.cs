3d87674febf6f8d70a641c5079783b1738e7bb53
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-05 22:05:05 +0700

    a

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Giai phuong trinh bac nhat");
+        Console.Write("Nhap a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Nhap b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        if (a != 0)
+        {
+            double ketqua = -b / a;
+            Console.Write("Ket qua la: {0}!", ketqua);
+        }
+        else
+        {
+            if (b == 0)
+            {
+                Console.Write("The solution is all x!");
+            }
+            else
+            {
+                Console.Write("Nod solution!");
+            }
+        }
+    }
+}
